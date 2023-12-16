646d8d85d2d3394a9e82d6450d6a40821ef8fe1c
xinxin.he <cs_hexinxin@163.com>
2023-12-17 07:20:22 +0800

    fix build multiple platform image in pipeline

--- app/scripts/build-multiple-platsform-image-cicd.sh
+++ app/scripts/build-multiple-platsform-image-cicd.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-echo "Start to build and push arm64 images to docker hub"
-docker buildx build  -t csmervyn718/springboot-java-project-template:latest --platform=linux/arm64 . --push
-echo "ARM64 Images push to docker hub success"
-echo "Start to build and push amd64 images to docker hub"
-docker buildx build  -t csmervyn718/springboot-java-project-template:latest --platform=linux/amd64 . --push
-echo "AMD64 Images push to docker hub success"
+echo "Start to build and push images to docker hub"
+docker buildx build  -t csmervyn718/springboot-java-project-template:latest --platform=linux/arm64,linux/amd64 . --push
+echo "Images push to docker hub success ✅"
